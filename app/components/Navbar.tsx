df9c800ec2f2797b66d1eda4968f522708a99e3c
muaath-rifath <muaathrifath@gmail.com>
2026-06-28 03:15:36 +0530

    fix(logo): resolve responsive spacing overlap by locking SVG text length and calculating precise transform gap

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="mx-auto flex h-16 max-w-[var(--container-max)] items-center justify-between px-4 sm:px-[var(--spacing-gutter)] md:px-10">
         <div className="flex items-center gap-[var(--spacing-gutter)] w-full">
           <Link href="/" className="flex items-center flex-shrink-0">
-            <Image src="/erlanglabs-logo-topbar.svg" alt="ErlangLabs Logo" width={700} height={140} className="h-8 w-auto object-contain" />
+            <Image src="/erlanglabs-logo-topbar.svg" alt="ErlangLabs Logo" width={700} height={140} className="h-auto w-36 sm:w-44 object-contain" />
           </Link>
           <div className="flex-1 flex justify-end lg:justify-start items-center">
              <ClientNav />
